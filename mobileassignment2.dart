5ea33f4adac4e900aa037056537a7a936c072744
SworupTimalsina <112379783+SworupTimalsina@users.noreply.github.com>
2024-11-17 13:32:22 +0545

    second question

---
+++ mobileassignment2.dart
@@ -0,0 +1,9 @@
+//Program to Check Even or Odd
+
+void main() {
+    String result=checkEvenOdd(8);
+    print("The number is ");
+}
+string checkEvenOdd(int number) {
+    return(number%2=0)? "Even":"Odd"
+}
